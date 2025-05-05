4865085c62adfbf2773a5172ceb75ef8851b986a
minagishl <mvxd568.co@icloud.com>
2025-05-05 16:25:47 +0900

    chore: add eslint plugin for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,6 +17,7 @@ const eslintConfig = [
       '@next/next/no-img-element': 'off',
     },
   },
+  prettier,
 ];
 
 export default eslintConfig;
