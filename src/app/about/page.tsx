e8589d0aea6c3a179257ad7b460ab51a3b7ba46a
minagishl <mvxd568.co@icloud.com>
2025-05-09 02:50:55 +0900

    feat: create about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,24 +2,57 @@
 
 import Link from 'next/link';
 import { Layout } from '@/components/layout/Layout';
+import { useEffect, useState } from 'react';
 
 export default function About() {
+  const [license, setLicense] = useState<string>('');
+
+  useEffect(() => {
+    const fetchLicense = async () => {
+      try {
+        const response = await fetch(
+          'https://raw.githubusercontent.com/minagishl/nostro/refs/heads/main/LICENSE',
+        );
+        const text = await response.text();
+        setLicense(text);
+      } catch (error) {
+        console.error('Failed to fetch license:', error);
+      }
+    };
+
+    fetchLicense();
+  }, []);
+
   return (
     <Layout>
       <div className="w-full">
-        <div className="mb-10 flex w-full flex-col space-y-6 rounded-xl bg-white p-8 shadow-lg dark:bg-gray-800">
-          <h2 className="mb-2 text-2xl font-bold text-gray-900 dark:text-white">About Nostro</h2>
+        <div className="mb-10 flex w-full flex-col space-y-6 p-4">
           <div className="space-y-6">
             <div className="space-y-2">
-              <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">Developer</h3>
-              <Link
-                href="https://github.com/minagishl"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-              >
-                @minagishl
-              </Link>
+              <h3 className="mb-2 text-lg font-semibold text-gray-800 dark:text-gray-200">About</h3>
+              <p className="text-gray-600 dark:text-gray-400">
+                This client was created by{' '}
+                <Link
+                  href="https://github.com/minagishl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-indigo-600 hover:text-indigo-600 dark:text-indigo-400 dark:hover:text-indigo-400"
+                >
+                  @minagishl
+                </Link>
+              </p>
+              {license && (
+                <div className="mt-4">
+                  <h3 className="mb-2 text-lg font-semibold text-gray-800 dark:text-gray-200">
+                    License
+                  </h3>
+                  <div className="rounded bg-gray-50 px-4 dark:bg-gray-800">
+                    <pre className="overflow-x-scroll bg-gray-50 py-4 text-gray-600 dark:bg-gray-800 dark:text-gray-400">
+                      {license}
+                    </pre>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
