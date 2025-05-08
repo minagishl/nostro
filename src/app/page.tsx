f3b10dc5977851f3e0f8112078871862d27a3553
minagishl <mvxd568.co@icloud.com>
2025-05-09 01:57:04 +0900

    feat: add Head component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@
 
 import { Layout } from '@/components/layout/Layout';
 import { Timeline } from '@/components/nostr/Timeline';
+import { Head } from '@/components/ui/Head';
+import { HomeIcon } from 'lucide-react';
 
 export default function Home() {
   return (
     <Layout>
-      <div className="flex flex-col space-y-6">
+      <div className="flex flex-col">
+        <Head icon={HomeIcon} title="Home" />
         <Timeline />
       </div>
     </Layout>
