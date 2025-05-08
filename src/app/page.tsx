98bd16c460a31facc3af215db70515b7d9aa49ed
minagishl <mvxd568.co@icloud.com>
2025-05-09 02:01:00 +0900

    feat: remove Head component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,11 @@
 
 import { Layout } from '@/components/layout/Layout';
 import { Timeline } from '@/components/nostr/Timeline';
-import { Head } from '@/components/ui/Head';
-import { HomeIcon } from 'lucide-react';
 
 export default function Home() {
   return (
     <Layout>
       <div className="flex flex-col">
-        <Head icon={HomeIcon} title="Home" />
         <Timeline />
       </div>
     </Layout>
